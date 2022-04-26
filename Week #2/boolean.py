5178365b627b98fa61b8d7ca53291c9b3e7af97b
AlimAlb <alimalbogachiev@yandex.ru>
2022-04-26 11:10:54 +0300

    second week update

---
+++ Week #2/boolean.py
@@ -0,0 +1,11 @@
+#and, or, not, разобрать все на доске
+#операторы сравнения
+print('Enter first number:')
+a = int(input())
+print('Enter second number:')
+b = int(input())
+bool = a > b
+print("a>b:")
+print(bool)
+
+#написать программу которая определяет входит ли число в промежуток
